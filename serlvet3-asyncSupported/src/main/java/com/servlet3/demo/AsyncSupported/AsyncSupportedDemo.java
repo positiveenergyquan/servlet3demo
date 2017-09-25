68137f80db94f39052a4507fc7a7b3c714116b82
favour <734501318@qq.com>
2017-09-25 08:45:54 +0800

    Servlet3.0 异步操作

---
+++ serlvet3-asyncSupported/src/main/java/com/servlet3/demo/AsyncSupported/AsyncSupportedDemo.java
@@ -0,0 +1,36 @@
+package com.servlet3.demo.AsyncSupported;
+
+import com.servlet3.demo.util.Executor;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(value = "/demo",asyncSupported = true)
+public class AsyncSupportedDemo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("进入Servlet的时间：" + new Date() + ".");
+        out.flush();
+
+        //在子线程中执行业务调用，并由其负责输出响应，主线程退出
+        AsyncContext ctx = req.startAsync();
+        new Thread(new Executor(ctx)).start();
+
+        out.println("结束Servlet的时间：" + new Date() + ".");
+        out.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ serlvet3-asyncSupported/src/main/java/com/servlet3/demo/util/Executor.java
@@ -0,0 +1,26 @@
+package com.servlet3.demo.util;
+
+import javax.servlet.AsyncContext;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class Executor implements Runnable {
+    private AsyncContext ctx = null;
+
+    public Executor(AsyncContext ctx) {
+        this.ctx = ctx;
+    }
+
+    public void run() {
+        try {
+            //等待十秒钟，以模拟业务方法的执行
+            Thread.sleep(10000);
+            PrintWriter out = ctx.getResponse().getWriter();
+            out.println("业务处理完毕的时间：" + new Date() + ".");
+            out.flush();
+            ctx.complete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
